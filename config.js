9656434d64d9c8d459dfc406c55c6732095ef0de
aruneshbal <aruneshbal@outlook.com>
2020-10-31 17:04:21 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+import*as firebase from'firebase'
+var firebaseConfig = {
+    apiKey: "AIzaSyAcrlLax6u9uyCOa-0Mkt0sOcgS3bPdXYU",
+    authDomain: "willi-1c434.firebaseapp.com",
+    databaseURL: "https://willi-1c434.firebaseio.com",
+    projectId: "willi-1c434",
+    storageBucket: "willi-1c434.appspot.com",
+    messagingSenderId: "652048053758",
+    appId: "1:652048053758:web:51aa0e89a7d784a1243caf"
+  };
